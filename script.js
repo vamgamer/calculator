fbd1ce7b1c44fe08e658918749956586e7fc7f64
vamgamer <115734039+vamgamer@users.noreply.github.com>
2023-03-12 17:27:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function insert(x){
+    var numero = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = numero + x;
+}
+function clian(){
+    document.getElementById('display').innerHTML = ""
+}
+function back(){
+    var back = document.getElementById('display').innerHTML;
+    document.getElementById('display').innerHTML = back.substring(0, back.length -1);
+}
+function calcular(){
+    var display = document.getElementById('display').innerHTML;
+    if(display){
+        document.getElementById('display').innerHTML = eval(display);
+    }
+}
